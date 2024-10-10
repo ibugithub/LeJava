ae84e119fd175e9aadc25570a23a6a26ebb896f1
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-10-10 13:05:27 +0600

    experiment until looping video

---
+++ Intro.java
@@ -0,0 +1,16 @@
+class Intro {
+
+  public static void main(String a[]) {
+    int b = 50;
+    int c = 800;
+    int d = 190;
+
+    if (b > c && b > d)
+      System.out.println(b);
+    else if (c > b && c > d)
+      System.out.println(c);
+    else 
+      System.out.println(d);
+
+  }
+}
